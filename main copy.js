341e57bf265ef89b51003f9cedf933d13564c9e8
OojivsoO <49951479+OojivsoO@users.noreply.github.com>
2022-11-14 18:57:58 +0800

    modulize

--- main copy.js
+++ main copy.js
@@ -2,25 +2,18 @@ import * as THREE from 'three';
 
 import Stats from 'three/addons/libs/stats.module.js';
 
+import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
+import { ImprovedNoise } from 'three/addons/math/ImprovedNoise.js';
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
-
-import { MyMinecraftControls } from './src/MyMinecraftControls.js'
-import { World } from './src/World.js' 
 
 let container, stats;
 
-let camera, controls, scene, renderer, world, geometry, mesh, texture;
+let camera, controls, scene, renderer;
 
 const worldWidth = 128, worldDepth = 128;
 const worldHalfWidth = worldWidth / 2;
 const worldHalfDepth = worldDepth / 2;
-
-const pxGeometry = new THREE.PlaneGeometry( 100, 100 );
-const nxGeometry = new THREE.PlaneGeometry( 100, 100 );
-const pyGeometry = new THREE.PlaneGeometry( 100, 100 );
-const pzGeometry = new THREE.PlaneGeometry( 100, 100 );
-const nzGeometry = new THREE.PlaneGeometry( 100, 100 );
-const matrix = new THREE.Matrix4();
+const data = generateHeight( worldWidth, worldDepth );
 
 const clock = new THREE.Clock();
 
@@ -37,81 +30,48 @@ function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xbfd1e5 );
 
-    world = new World(worldWidth, worldDepth);
+    // sides
 
-    initGeo();
+    const matrix = new THREE.Matrix4();
 
-    texture = new THREE.TextureLoader().load( 'textures/atlas.png' );
-    texture.magFilter = THREE.NearestFilter;
-
-    //
-    geometry = worldToGeometry(world)
-    geometry.computeBoundingSphere();
-
-    mesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { map: texture, side: THREE.DoubleSide } ) );
-    scene.add( mesh );
-
-    const ambientLight = new THREE.AmbientLight( 0xcccccc );
-    scene.add( ambientLight );
-
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
-    directionalLight.position.set( 1, 1, 0.5 ).normalize();
-    scene.add( directionalLight );
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
-
-    controls = new MyMinecraftControls( camera, renderer.domElement, scene, world, updateMeshCallback );
-
-    controls.movementSpeed = 1000;
-    controls.lookSpeed = 0.125;
-    controls.lookVertical = true;
-
-    stats = new Stats();
-    container.appendChild( stats.dom );
-
-    //
-
-    window.addEventListener( 'resize', onWindowResize );
-
-}
-
-function initGeo(){
+    const pxGeometry = new THREE.PlaneGeometry( 100, 100 );
     pxGeometry.attributes.uv.array[ 1 ] = 0.5;
     pxGeometry.attributes.uv.array[ 3 ] = 0.5;
     pxGeometry.rotateY( Math.PI / 2 );
     pxGeometry.translate( 50, 0, 0 );
 
+    const nxGeometry = new THREE.PlaneGeometry( 100, 100 );
     nxGeometry.attributes.uv.array[ 1 ] = 0.5;
     nxGeometry.attributes.uv.array[ 3 ] = 0.5;
     nxGeometry.rotateY( - Math.PI / 2 );
     nxGeometry.translate( - 50, 0, 0 );
 
+    const pyGeometry = new THREE.PlaneGeometry( 100, 100 );
     pyGeometry.attributes.uv.array[ 5 ] = 0.5;
     pyGeometry.attributes.uv.array[ 7 ] = 0.5;
     pyGeometry.rotateX( - Math.PI / 2 );
     pyGeometry.translate( 0, 50, 0 );
 
+    const pzGeometry = new THREE.PlaneGeometry( 100, 100 );
     pzGeometry.attributes.uv.array[ 1 ] = 0.5;
     pzGeometry.attributes.uv.array[ 3 ] = 0.5;
     pzGeometry.translate( 0, 0, 50 );
 
+    const nzGeometry = new THREE.PlaneGeometry( 100, 100 );
     nzGeometry.attributes.uv.array[ 1 ] = 0.5;
     nzGeometry.attributes.uv.array[ 3 ] = 0.5;
     nzGeometry.rotateY( Math.PI );
     nzGeometry.translate( 0, 0, - 50 );
-}
 
-function worldToGeometry(){
+    //
+
     const geometries = [];
 
     for ( let z = 0; z < worldDepth; z ++ ) {
 
         for ( let x = 0; x < worldWidth; x ++ ) {
 
-            const h = world.getY( x, z );
+            const h = getY( x, z );
 
             matrix.makeTranslation(
                 x * 100 - worldHalfWidth * 100,
@@ -127,33 +87,66 @@ function worldToGeometry(){
             geometries.push( pyGeometry.clone().applyMatrix4( matrix ) );
 
             if ( ( px !== h && px !== h + 1 ) || x === 0 ) {
+
                 geometries.push( pxGeometry.clone().applyMatrix4( matrix ) );
+
             }
 
             if ( ( nx !== h && nx !== h + 1 ) || x === worldWidth - 1 ) {
+
                 geometries.push( nxGeometry.clone().applyMatrix4( matrix ) );
+
             }
 
             if ( ( pz !== h && pz !== h + 1 ) || z === worldDepth - 1 ) {
+
                 geometries.push( pzGeometry.clone().applyMatrix4( matrix ) );
+
             }
 
             if ( ( nz !== h && nz !== h + 1 ) || z === 0 ) {
+
                 geometries.push( nzGeometry.clone().applyMatrix4( matrix ) );
+
             }
 
         }
 
     }
 
-    return geometry = BufferGeometryUtils.mergeBufferGeometries( geometries );
-}
+    const geometry = BufferGeometryUtils.mergeBufferGeometries( geometries );
+    geometry.computeBoundingSphere();
 
-function updateMeshCallback(){
-    scene.remove(mesh);
-    mesh.geometry.dispose();
-    mesh.material.dispose();
-    geometry = worldToGeometry();
+    const texture = new THREE.TextureLoader().load( 'textures/atlas.png' );
+    texture.magFilter = THREE.NearestFilter;
+
+    const mesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { map: texture, side: THREE.DoubleSide } ) );
+    scene.add( mesh );
+
+    const ambientLight = new THREE.AmbientLight( 0xcccccc );
+    scene.add( ambientLight );
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
+    directionalLight.position.set( 1, 1, 0.5 ).normalize();
+    scene.add( directionalLight );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    controls = new FirstPersonControls( camera, renderer.domElement );
+
+    controls.movementSpeed = 1000;
+    controls.lookSpeed = 0.125;
+    controls.lookVertical = true;
+
+    stats = new Stats();
+    container.appendChild( stats.dom );
+
+    //
+
+    window.addEventListener( 'resize', onWindowResize );
 
 }
 
@@ -165,6 +158,39 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 
     controls.handleResize();
+
+}
+
+function generateHeight( width, height ) {
+
+    const data = [], perlin = new ImprovedNoise(),
+        size = width * height, z = Math.random() * 100;
+
+    let quality = 2;
+
+    for ( let j = 0; j < 4; j ++ ) {
+
+        if ( j === 0 ) for ( let i = 0; i < size; i ++ ) data[ i ] = 0;
+
+        for ( let i = 0; i < size; i ++ ) {
+
+            const x = i % width, y = ( i / width ) | 0;
+            data[ i ] += perlin.noise( x / quality, y / quality, z ) * quality;
+
+
+        }
+
+        quality *= 4;
+
+    }
+
+    return data;
+
+}
+
+function getY( x, z ) {
+
+    return ( data[ x + z * worldWidth ] * 0.15 ) | 0;
 
 }
 
