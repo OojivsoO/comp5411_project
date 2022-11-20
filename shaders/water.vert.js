739336f9523f39f5a911477b617af68d90b8e9b1
codermomo <alexmo201898@gmail.com>
2022-11-20 21:19:10 +0800

    cleaned water vertex shader

--- shaders/water.vert.js
+++ shaders/water.vert.js
@@ -9,17 +9,9 @@ void main() {
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
-    // vertex normal
     worldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
     N = normalize((modelMatrix * vec4(normal, 1.0)).xyz);
-
-    // ray of reflection
-    //reflectedRay = reflect(worldPosition.xyz - cameraPosition, N);
-    //reflectedRay.x = -reflectedRay.x;
     camPosition = cameraPosition;
-
-    // uVu = (gl_Position.xy / gl_Position.w) / 2.0 + 0.5; // ndc as uv, 
-    // // sounds good but looks strange as texture is 'sticked' to the screen - useful for effects like water droplets on camera
     uVu = uv;
 }
 
